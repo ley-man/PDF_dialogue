709a50299bef6bc443db194a46dbf18b2b7465c6
ley-man <pkumar14@stevens.edu>
2023-10-31 16:38:47 -0400

    Add Text Splitter

--- app.py
+++ app.py
@@ -1,1 +1,53 @@
 import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+from PyPDF2 import PdfReader
+from dotenv import find_dotenv, load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Create Sidebar
+with st.sidebar:
+    st.title("LLM Dialogue App 😊")
+    st.markdown('''
+    ## About
+    This app is designed to chat with your personal pdf articles and books.
+    Tools used:
+    - [Streamlit](https://streamlit.io/)
+    - [Langchain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/)
+    ''')
+    add_vertical_space(5)
+    st.write('❤️ Chat with PDFs ❤️')
+
+
+def main():
+    st.header("💬Hello LLMs🗨️")
+
+    # Take pdf file as input
+    pdf_file = st.file_uploader(
+        "Please upload your pdf file", type='pdf')
+    # TODO- Take 'url' user input
+
+    def get_text_embeddings(text, model="text-embedding-ada-002"):
+        print("")
+
+    # Read pdf
+    if pdf_file is not None:
+        pdf_reader = PdfReader(pdf_file)
+        st.write(pdf_reader)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+
+        chunks = text_splitter.split_text(text=text)
+        st.write(chunks)
+
+
+if __name__ == "__main__":
+    main()
